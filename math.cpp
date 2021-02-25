fdac6980d87cd66aafb0500aec8afaee64ab8b9e
Jameson Stahl <jstahl@carthage.edu>
2021-02-25 16:30:41 -0600

    Added two helper functions and fixed multiply() functionality

--- math.cpp
+++ math.cpp
@@ -1,7 +1,4 @@
 #include "math.h"
-#include <iostream>
-
-using namespace std;
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
@@ -15,44 +12,96 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 //--
 bool multiply(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len)
 {
-    //Checks inputs to make sure they are valid
+    //Checks inputs to make sure they are valid. Function returns false if inputs are not valid
     if(numerator1 < denominator1 && numerator2 < denominator2 && denominator1 != 0 && denominator2 != 0){
-        //Converting both inputted mixed numbers to improper fractions
+        
+        //Converts both inputted mixed numbers to improper fractions
         int improperNumerator1 = (characteristic1 * denominator1) + numerator1;
         int improperNumerator2 = (characteristic2 * denominator2) + numerator2;
         
-        //Multiplies the two improper fractions which results in the product
-        int productNumerator = (improperNumerator1 * improperNumerator2);
-        int productDenominator = (denominator1 * denominator2);
+        //Calculates the greatest common divisor(gcd) for both sets of numbers
+        int gcd1 = greatestCommonDivisor(improperNumerator1, denominator1);
+        int gcd2 = greatestCommonDivisor(improperNumerator2, denominator2);
         
-        //Converts the improper fraction to a mixed number
+        //Reduces the improper fractions by dividing by the gcd
+        improperNumerator1 = improperNumerator1 / gcd1;
+        improperNumerator2 = improperNumerator2 / gcd2;
+        denominator1 = denominator1 / gcd1;
+        denominator2 = denominator2 / gcd2;
+        
+        //Multiplies the two improper fractions which results in the product if there is no integer overflow. Function returns false if integer overflow occurs
+        int productNumerator = 0;
+        int productDenominator = 0;
+        if(isOverflow(improperNumerator1, improperNumerator2) == false){
+            productNumerator = (improperNumerator1 * improperNumerator2);
+        }
+        else{
+            return false;
+        }
+        if(isOverflow(denominator1, denominator2) == false){
+            productDenominator = (denominator1 * denominator2);
+        }
+        else{
+            return false;
+        }
+        
+        //Converts the resulted improper fraction to a mixed number
         int wholeNumber = (productNumerator / productDenominator);
         int mixedNumerator = (productNumerator % productDenominator);
         
-        //Converts the fraction
-        //Needs to be finished
-        int decimal = (mixedNumerator / productDenominator);
-        
-        //Converts wholeNumber and decimal to seperate arrays of chars
+        //Initializes two new char arrays to store the characteristic and mantissa
         char characteristic[len];
+        characteristic[0] = '\0';
         char mantissa[len];
-        sprintf(characteristic, "%d", wholeNumber);
-        sprintf(mantissa, "%d", decimal);
+        mantissa[0] = '\0';
         
-        //Checks the length of each array
-        int lengthOfCharacteristic = lengthOfArray(characteristic, len);
-        int lengthOfMantissa = lengthOfArray(mantissa, len);
+        //Adds each digit of the characteristic to the corresponding char array in reverse order due to modular divison
+        int digit = wholeNumber;
+        int counter1 = 0;
+        while(digit != 0 && counter1 != len){
+            int newDigit = digit % 10;
+            characteristic[counter1] = newDigit + 48;
+            digit = digit / 10;
+            counter1++;
+        }
+        characteristic[counter1] = '\0';
         
-        //Adds a decimal point to the characteristic array to seperate the whole number from the decimal
-        characteristic[lengthOfCharacteristic] = '.';
+        //Adds each digit of the mantissa to the corresponding char array
+        int newNumerator = mixedNumerator;
+        int counter2 = 0;
+        while(newNumerator != 0 && counter2 != len - 1){
+            newNumerator = newNumerator * 10;
+            if(newNumerator >= productDenominator){
+                int newDigit = newNumerator / productDenominator;
+                mantissa[counter2] = newDigit + 48;
+                newNumerator = newNumerator % productDenominator;
+                counter2++;
+            }
+            else{
+                mantissa[counter2] = 48;
+                counter2++;
+            }
+        }
+        mantissa[counter2] = '\0';
+        
+        //Checks the length used of each array
+        int lengthOfCharacteristic = lengthSoFar(characteristic, len);
+        int lengthOfMantissa = lengthSoFar(mantissa, len);
+        
+        //Copies characteristic array into result array by reversing the order of digits
+        int resultPosition = 0;
+        for(int i = lengthOfCharacteristic - 1; i >= 0; i--){
+            result[resultPosition] = characteristic[i];
+            resultPosition++;
+        }
+        
+        //Adds a decimal point to the result array to seperate the characteristic from the mantissa if the length of mantissa is greater than zero
+        if(lengthOfMantissa > 0){
+            result[lengthOfCharacteristic] = '.';
+        }
         
         //Updates lengthOfCharacteristicr to account for the decimal point
         lengthOfCharacteristic++;
-        
-        //Copies characteristic array into result array
-        for(int i = 0; i < lengthOfCharacteristic; i++){
-            result[i] = characteristic[i];
-        }
         
         //Checks to make sure the characteristic and mantissa will fit within the result array and adjusts the lengthOfMantissa if it will not fit
         if(lengthOfCharacteristic + lengthOfMantissa >= len){
@@ -60,22 +109,19 @@ bool multiply(int characteristic1, int numerator1, int denominator1, int charact
             lengthOfMantissa = lengthOfMantissa - difference;
         }
         
-        //Copies mantissa array into result array after the characteristic already in result
+        //Copies mantissa array into result array after the characteristic already in result array
         for(int i = 0; i < lengthOfMantissa; i++){
             result[i + lengthOfCharacteristic] = mantissa[i];
         }
+        result[len - 1] = '\0';
     }
     else{
         return false;
     }
     return true;
-    /*
-     Test code to be put in main():
-     multiply(3, 1, 4, 3, 1, 3, result, lengthOfResult);
-     */
 }
 
-int lengthOfArray(char array[], int len){
+int lengthSoFar(char array[], int len){
     //Loop to determine the length of the array so far
     int lengthSoFar = 0;
     for(int i = 0; i < len; i++){
@@ -88,6 +134,29 @@ int lengthOfArray(char array[], int len){
     }
     return lengthSoFar;
 }
+
+int greatestCommonDivisor(int numerator, int denominator){
+    //Recursively calls the function to return the greatest common divisor
+    if(denominator == 0){
+        return numerator;
+    }
+    return greatestCommonDivisor(denominator, numerator % denominator);
+}
+
+bool isOverflow(int value1, int value2){
+    //Checks to see if there is an int overflow
+    if (value1 == 0 || value2 == 0){
+        return false;
+    }
+    int result = value1 * value2;
+    if (value1 == result / value2){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
 //--
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
--- math.h
+++ math.h
@@ -6,4 +6,6 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool multiply(int characteristic1, int numerator1, int denominator1, int characteristic2, int numerator2, int denominator2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
-int lengthOfArray(char array[], int len);
+int lengthSoFar(char array[], int len);
+int greatestCommonDivisor(int numerator, int denominator);
+bool isOverflow(int value1, int value2);
