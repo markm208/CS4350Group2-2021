e61b35145e6b626f1823240676de08be8c7d723c
DarrylMcCottrell <43862357+DarrylMcCottrell@users.noreply.github.com>
2021-02-26 11:57:12 -0600

    Add implemented with Helper functions

--- math.h
+++ math.h
@@ -6,3 +6,7 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
+
+int count_digits(int numberToCount);
+bool mantissaToArray(int numerator, int denominator, int maxLength, int mantissaArray[]);
+void intToDigitArray(int number, int digitArray[]);
